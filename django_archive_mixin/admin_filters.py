e2a555af7a2ee8bf792f0cac2451a5f40a7a4860
Oliver Biwer <soraphis@users.noreply.github.com>
2017-09-04 14:35:51 +0200

    Update admin_filters.py

--- django_archive_mixin/admin_filters.py
+++ django_archive_mixin/admin_filters.py
@@ -1,15 +1,20 @@
 from django.contrib.admin import SimpleListFilter
 
-class ArchivedFilter(SimpleListFilter):
-    title = "Archived"
-    parameter_name = "deleted"
 
-    def lookups(self, request, model_admin):
-        return [("non", "Non-Archive"), ("arc", "Archive")]
+def get_ArchivedFilter(for_class):
+    
+    class ArchivedFilter(SimpleListFilter):
+        title = "Archived"
+        parameter_name = "deleted"
 
-    def queryset(self, request, queryset):
-        if self.value() == "non":
-            return queryset
-        if self.value() == "arc":
-            return WaitListEntry.objects.deleted
-        return WaitListEntry.all_objects.all()
+        def lookups(self, request, model_admin):
+            return [("non", "Non-Archive"), ("arc", "Archive")]
+
+        def queryset(self, request, queryset):
+            if self.value() == "non":
+                return queryset
+            if self.value() == "arc":
+                return for_class.objects.deleted
+            return for_class.all_objects.all()
+    
+    return ArchivedFilter
